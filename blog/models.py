f1d0c7c5894010e254a586531a7c840923e3a501
evgeniy-kulikov <redstepway@gmail.com>
2024-01-17 16:58:22 +0300

    blog create models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=250, verbose_name="URL slug")
+    body = models.TextField(verbose_name="Содержимое")
+    publish = models.DateTimeField(default=timezone.now, verbose_name="Дата публикации")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+    def __str__(self):
+        return self.title
