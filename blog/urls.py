32e1e495aa4dd0a99e045a0eac502a2a16dd1759
evgeniy-kulikov <redstepway@gmail.com>
2024-01-19 09:10:02 +0300

    6.3 Разработка представлений на основе классов

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
 
+    # FBV
+    # path('', views.post_list, name='post_list'),
     # Получение адреса через параметры publish.year/publish.month/publish.day/slug
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
           views.post_detail,
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,52 @@
 from django.shortcuts import render, get_object_or_404
 # from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 
+# Переопределние класса Paginator
+"""
+При превышении диапазона страниц - выдает последнюю
+При занижении (ноль и минус) диапазона страниц - выдает первую
+При нечисловом значении выдает ошибку 404
+"""
+class MyPaginator(Paginator):
+    def validate_number(self, number):
+        try:
+            return super().validate_number(number)
+        except EmptyPage:
+            if int(number) > 1:
+                # return the last page
+                return self.num_pages
+            elif int(number) < 1:
+                # return the first page
+                return 1
+            else:
+                raise
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+    paginator_class = MyPaginator
+
+
+# При неверных номерах страницы выдает ошибку 404
+# class PostListView(ListView):
+#     # model = Post  # Будет выборка Post.objects.all()
+#     queryset = Post.published.all() # Своя выборка
+#     # Свое наименование, вместо умолчальной object_list
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     # Шаблон по умолчанию  blog/post_list.html
+#     template_name = 'blog/post/list.html'
+
+
+
+""" FBV """
 def post_list(request):
     posts_all = Post.published.all()
     # posts = Post.objects.all()
