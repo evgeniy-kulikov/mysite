8cf4bde8686f64d62541f2cf1b65b601eb96a6d9
evgeniy-kulikov <redstepway@gmail.com>
2024-01-24 13:42:26 +0300

    6.10 Установка PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,10 +104,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("NAME_PG"),
+        'USER': os.getenv("USER_PQ"),
+        'PASSWORD': os.getenv("PASSWORD_PG"),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
