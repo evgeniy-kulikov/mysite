4346843151a6f83b93d05374c99d30f222bdda67
evgeniy-kulikov <redstepway@gmail.com>
2024-01-26 12:58:11 +0300

    7.3 Доработка системы регистрации

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,9 +4,13 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=30, label='Имя')
+    first_name = forms.CharField(max_length=100, label='Имя')
+    last_name = forms.CharField(max_length=100, label='Фамилия')
+    username = forms.CharField(max_length=30, label='Логин')
     email = forms.EmailField(max_length=200)
+    password1 = forms.CharField(max_length=50, widget=forms.PasswordInput(), label='Пароль')
+    password2 = forms.CharField(max_length=50, widget=forms.PasswordInput(), label='Повторить пароль')
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
