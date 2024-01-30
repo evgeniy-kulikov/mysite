3b263a884dcbacfdf8365f346048802f8b4d615b
evgeniy-kulikov <redstepway@gmail.com>
2024-01-30 18:36:47 +0300

    8.5 OrderingFilter

--- blog_api/views.py
+++ blog_api/views.py
@@ -3,24 +3,50 @@ from blog.models import Post
 from .serializers import PostSerializer
 
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
+
+
+# OrderingFilter
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    # модельное поле, которое используется для поиска объектов отдельных экземпляров модели. По умолчанию 'pk'
-    # lookup_field = 'pk'
-    filter_backends = [DjangoFilterBackend]  # pip django_filters
-    filterset_fields = ['author']  # pip django_filters
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['author']
+    search_fields = ['body', 'author__username']
+    ordering_fields = ['author_id', 'publish']
 
-    """ 
-    При необходимости можно сделать добавление фильтрации 
-    Самый простой способ отфильтровать QuerySet любого представления, подкласса GenericAPIView, 
-    - это переопределить метод .get_queryset()
-    """
-    # Отфильтровать набор запросов по текущему аутентифицированному пользователю
-    def get_queryset(self):
-        user = self.request.user
-        return Post.objects.filter(author=user)
+    # ordering_fields = '__all__'  # упорядочивание по любому полю модели
+    # ordering = ['body']  # строка, или спискок/кортеж строк
+
+
+# # SearchFilter
+# class PostList(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+#     filterset_fields = ['author']
+#     search_fields = ['body']
+
+
+
+# class PostList(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#     # модельное поле, которое используется для поиска объектов отдельных экземпляров модели. По умолчанию 'pk'
+#     # lookup_field = 'pk'
+#     filter_backends = [DjangoFilterBackend]  # pip django_filters
+#     filterset_fields = ['author']  # pip django_filters
+#
+#     """
+#     При необходимости можно сделать добавление фильтрации
+#     Самый простой способ отфильтровать QuerySet любого представления, подкласса GenericAPIView,
+#     - это переопределить метод .get_queryset()
+#     """
+#     # Отфильтровать набор запросов по текущему аутентифицированному пользователю
+#     def get_queryset(self):
+#         user = self.request.user
+#         return Post.objects.filter(author=user)
 
 
 
